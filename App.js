0304493c6d051cc72016cb9308c690a20b8516d4
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-17 16:50:46 -0500

    added safe area view and fragments

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View
+          style={{
+            padding: 16,
+            justifyContent: "left",
+            backgroundColor: "green",
+          }}
+        >
+          <Text>search</Text>
+        </View>
+        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ 
 });
