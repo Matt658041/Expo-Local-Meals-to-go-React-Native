0d2736899b5985361cabd0912111c0ec961f0bd7
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-18 13:21:52 -0500

    added components and restaurant components

--- App.js
+++ App.js
@@ -1,35 +1,13 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import {
-  StatusBar,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-} from "react-native";
+import React from "react";
 
-
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight  }}>
-        <View
-          style={{
-            padding: 16,
-            justifyContent: "left",
-            backgroundColor: "green",
-          }}
-        >
-          <Text>search</Text>
-        </View>
-        <View style={{ flex: 1, padding: 16, backgroundColor: "blue" }}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({});
---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text } from "react-native";
+
+export const RestaurantInfo = () => (
+<Text>Restaurant Info</Text>
+
+)
