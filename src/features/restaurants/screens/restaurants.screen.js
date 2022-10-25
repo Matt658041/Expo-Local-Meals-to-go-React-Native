6761aae1bbec3f29387929200ad9e6a1e71866ea
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-25 12:57:17 -0500

    added mock location and location service

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,62 +1,57 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, FlatList, StyleSheet } from "react-native";
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
-import { Spacer } from "../../../components/spacer/spacer.component";
+import { ActivityIndicator, Colors } from "react-native-paper";
+
 import { SafeArea } from "../../../components/utility/safe-area.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
-
 const SearchContainer = styled.View`
-padding: ${(props) => props.theme.space[3]};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
-    padding: 16
-  }
-})`
+    padding: 16,
+  },
+})``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
 `;
 
-
 export const RestaurantsScreen = () => {
-  const {isLoading, error, restaurants} = useContext(RestaurantsContext)
- 
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
-  <SafeArea>
-    <SearchContainer>
-      <Searchbar />
-    </SearchContainer>
-    <RestaurantList
-      data={restaurants}
+    <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true}  />
+        </LoadingContainer>
+      )}
+      <SearchContainer>
+        <Searchbar />
+      </SearchContainer>
+      <RestaurantList
+        data={restaurants}
         renderItem={({ item }) => {
-          console.log(item);
-         return (
+          return (
             <Spacer position="bottom" size="large">
               <RestaurantInfoCard restaurant={item} />
             </Spacer>
-          )
+          );
         }}
-      keyExtractor={(item) => item.name}
-      
-    />
-  </SafeArea>
-  )
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
