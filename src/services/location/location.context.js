a1a953ffc2a093ca766c1edee6921885bc4995a4
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-26 09:20:10 -0500

    added search components

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -5,21 +5,24 @@ import { locationRequest, locationTransform } from "./location.service";
 export const LocationContext = React.createContext();
 
 export const LocationContextProvider = ({ children }) => {
-  const [keyword, setKeyword] = useState("san francisco");
+  const [keyword, setKeyword] = useState("Kitty Village");
   const [location, setLocation] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const onSearch = (searchKeyword = "Antwerp") => {
-    console.log(searchKeyword);
+  const onSearch = (searchKeyword) => {
+    
     setIsLoading(true);
     setKeyword(searchKeyword);
+    if (!searchKeyword.length) {
+      return;
+    }
     locationRequest(searchKeyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
         setIsLoading(false);
         setLocation(result);
-        console.log(result);
+        
       })
       .catch((err) => {
         setIsLoading(false);
@@ -27,9 +30,7 @@ export const LocationContextProvider = ({ children }) => {
       });
   };
 
-  useEffect(() => {
-    onSearch();
-  }, []);
+
 
   return (
     <LocationContext.Provider
