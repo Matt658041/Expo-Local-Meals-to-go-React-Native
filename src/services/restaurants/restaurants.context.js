d350f2a010f5a37d35485c760fd8cb94541fc9dd
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-26 12:14:34 -0500

    added location to restaurant context

--- src/services/restaurants/restaurants.context.js
+++ src/services/restaurants/restaurants.context.js
@@ -1,5 +1,5 @@
-import React, { useState, createContext, useEffect, useMemo } from "react";
-
+import React, { useState, createContext, useEffect, useMemo, useContext } from "react";
+import { LocationContext } from "../location/location.context";
 import {
   restaurantsRequest,
   restaurantsTransform,
@@ -11,11 +11,12 @@ export const RestaurantsContextProvider = ({ children }) => {
   const [restaurants, setRestaurants] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const {location} = useContext(LocationContext)
 
-  const retrieveRestaurants = () => {
+  const retrieveRestaurants = (loc) => {
     setIsLoading(true);
     setTimeout(() => {
-      restaurantsRequest()
+      restaurantsRequest(loc)
         .then(restaurantsTransform)
         .then((results) => {
           setIsLoading(false);
@@ -28,7 +29,12 @@ export const RestaurantsContextProvider = ({ children }) => {
     }, 2000);
   };
   useEffect(() => {
-    retrieveRestaurants();
+  if (location)
+  {
+    console.log(location);
+    const locationString = `${location.lat}, ${location.lng}`;
+      retrieveRestaurants(locationString)
+    };
   }, []);
 
   return (
