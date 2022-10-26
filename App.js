a2fcf6d1a804d5356a1a716fdafb4a3ab8cf8b68
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-26 13:46:13 -0500

    refactored tab navigation and setting up stack navigation

--- App.js
+++ App.js
@@ -1,57 +1,28 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-import { theme } from "./src/infrastructure/theme";
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
-import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
-import { Text, View } from 'react-native';
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { SafeArea } from "./src/components/utility/safe-area.component";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { RestaurantsContextProvider} from './src/services/restaurants/restaurants.context'
+
+import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
+
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 
-const Tab = createBottomTabNavigator();
-
-const TAB_iCON = {
-  Restaurants: "md-restaurant",
-  Map: 'md-map',
-  Settings: "md-settings"
-};
-
-const Settings = () => (
-  <SafeArea>
-    <Text>Settings</Text>
-  </SafeArea>
-);
-const Map = () => (
-  <SafeArea>
-    <Text>Map</Text>
-  </SafeArea>
-);
-
-
-const createScreenOptions = ({ route }) => {
-  const iconName = TAB_iCON[route.name];
-  return {
-    tabBarIcon: ({size, color }) => (
-      <Ionicons name={iconName} size={size} color={color} />
-    ),
-  };
-};
-
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
 
- const [oswaldLoaded] = useOswald({
-   Oswald_400Regular,
- });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
 
-   const [ latoLoaded] = useLato({
-     Lato_400Regular,
-   });
-  
   if (!oswaldLoaded || !latoLoaded) {
     return null;
   }
@@ -61,13 +32,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <LocationContextProvider>
           <RestaurantsContextProvider>
-            <NavigationContainer>
-              <Tab.Navigator screenOptions={createScreenOptions}>
-                <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-                <Tab.Screen name="Map" component={Map} />
-                <Tab.Screen name="Settings" component={Settings} />
-              </Tab.Navigator>
-            </NavigationContainer>
+            <Navigation />
           </RestaurantsContextProvider>
         </LocationContextProvider>
       </ThemeProvider>
---
+++ src/infrastructure/navigation/index.js
@@ -0,0 +1,6 @@
+import react from "react";
+import { AppNavigator } from "./app.navigator";
+
+export const Navigation = () => {
+    return <AppNavigator />
+};
