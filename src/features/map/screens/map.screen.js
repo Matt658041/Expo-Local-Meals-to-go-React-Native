7af715cb92b4113913a19570a8b74814749eee55
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-28 12:42:01 -0500

    added navigation to onpress event

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,63 +1,67 @@
 import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
-import { Search } from '../components/search.component';
+
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+
+import { Search } from "../components/search.component";
 import { MapCallout } from "../components/map-callout.component";
 
 const Map = styled(MapView)`
-height: 100%
-width: 100%
-`
+  height: 100%;
+  width: 100%;
+`;
 
+export const MapScreen = ({ navigation }) => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
 
-export const MapScreen = () => {
-    const { location } = useContext(LocationContext);
-    const { restaurants = [] } = useContext(RestaurantsContext); 
+  const [latDelta, setLatDelta] = useState(0);
 
-    const [latDelta, setLatDelta] = useState(0);
+  const { lat, lng, viewport } = location;
 
-    const { lat, lng, viewport } = location;
-    
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
 
-    useEffect(() => { 
-        const northeastLat = viewport.northeast.lat;
-        const southwestLat = viewport.southwest.lat;
-        
-        setLatDelta(northeastLat - southwestLat)
-    }, [location, viewport]);
-    
-    return (
-        <>
-            <Search />
-            <Map
-            
-                region={{
-                    latitude: lat,
-                    longitude: lng,
-                    latitudeDelta: latDelta,
-                    longitudeDelta: 0.02,
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
 
-            }}>
-                {restaurants.map((restaurant) => {
-                    return (
-                      <MapView.Marker
-                        key={restaurant.name}
-                        title={restaurant.name}
-                        coordinate={{
-                          latitude: restaurant.geometry.location.lat,
-                          longitude: restaurant.geometry.location.lng,
-                        }}
-                      >
-                        <MapView.Callout
-                        onPress={() => null}>
-                         <MapCallout restaurant={restaurant}/>
-                        </MapView.Callout>
-                      </MapView.Marker>
-                    );               
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return (
+            <MapView.Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            >
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetail", {
+                    restaurant,
+                  })
+                }
+              >
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
+          );
         })}
-            </Map> 
-        </>
-    )
+      </Map>
+    </>
+  );
 };
