82e8a044cac80250a270814c476770ea9279df16
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-28 10:24:28 -0500

    create mapview markers and call outs

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -2,15 +2,63 @@ import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
 import { Search } from '../components/search.component';
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { View } from "react-native";
 
 const Map = styled(MapView)`
 height: 100%
 width: 100%
 `
+const SomeText = styled.Text`
 
-export const MapScreen = () => (
-    <>
-    <Search/>
-    <Map/>
-    </>
-);
+`;
+
+export const MapScreen = () => {
+    const { location } = useContext(LocationContext);
+    const { restaurants = [] } = useContext(RestaurantsContext); 
+
+    const [latDelta, setLatDelta] = useState(0);
+
+    const { lat, lng, viewport } = location;
+    
+
+    useEffect(() => { 
+        const northeastLat = viewport.northeast.lat;
+        const southwestLat = viewport.southwest.lat;
+        
+        setLatDelta(northeastLat - southwestLat)
+    }, [location, viewport]);
+    
+    return (
+        <>
+            <Search />
+            <Map
+            
+                region={{
+                    latitude: lat,
+                    longitude: lng,
+                    latitudeDelta: latDelta,
+                    longitudeDelta: 0.02,
+
+            }}>
+                {restaurants.map((restaurant) => {
+                    return (
+                      <MapView.Marker
+                        key={restaurant.name}
+                        title={restaurant.name}
+                        coordinate={{
+                          latitude: restaurant.geometry.location.lat,
+                          longitude: restaurant.geometry.location.lng,
+                        }}
+                      >
+                        <MapView.Callout>
+                         
+                        </MapView.Callout>
+                      </MapView.Marker>
+                    );               
+        })}
+            </Map> 
+        </>
+    )
+};
