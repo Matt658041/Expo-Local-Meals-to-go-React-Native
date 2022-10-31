8a930a91462ff00a31a9c22e24e4a98014ae33b4
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-31 09:24:21 -0500

    added favourites context js

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,12 +1,30 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const FavouritesContext = createContext();
 
-export const FavouritesContextProvider = ({children}) => {
-    return (
-        <FavouritesContext.Provider>
-            {children}
-        </FavouritesContext.Provider>
+export const FavouritesContextProvider = ({ children }) => {
+  const [favourites, setFavourites] = useState([]);
 
-    )
-}
+  const add = (restaurant) => {
+    setFavourites([...favourites, restaurant]);
+  };
+
+  const remove = (restaurant) => {
+    const newFavourites = favourites.filter(
+      (x) => x.placeId !== restaurant.placeId
+    );
+
+    setFavourites(newFavourites);
+  };
+  return (
+    <FavouritesContext.Provider
+      value={{
+        favourites,
+        addToFavourites: add,
+        removeFromFavourites: remove,
+      }}
+    >
+      {children}
+    </FavouritesContext.Provider>
+  );
+};
