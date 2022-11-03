82b70947fb834f178026e0d68e023811f1475692
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-03 12:25:49 -0500

    added styled image background for account styles screen

--- src/features/account/screens/register.screen.js
+++ src/features/account/screens/register.screen.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import { AccountBackground } from "../components/account.styles";
+import { AccountBackground, AccountCover } from "../components/account.styles";
 
 export const RegisterScreen = () => {
-  return <AccountBackground />;
+    return <AccountBackground>
+      <AccountCover/>
+  </AccountBackground>;
 };
