6984f43aba63b576d4ef4191cc53ff6cdb254b7c
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-03 13:27:24 -0500

    created login and register screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -3,17 +3,21 @@ import { AccountBackground, AccountContainer, AccountCover, AuthButton  } from "
 import { Spacer } from "../../../components/spacer/spacer.component";
     
     
-export const AccountScreen = () => {
+export const AccountScreen = ({navigation}) => {
     return <AccountBackground> 
         <AccountCover />
         <AccountContainer>
             <AuthButton icon="lock-open-outline"
-      mode="contained" onPress={() => console.log('Pressed')}>
+                mode="contained"
+                onPress={() =>
+                navigation.navigate('Login')}>
     Login
             </AuthButton>
             <Spacer size="large">
             <AuthButton icon="lock-open-outline"
-      mode="contained" onPress={() => console.log('Pressed')}>
+                    mode="contained"
+                    onPress={() =>
+                    navigation.navigate('Register')}>
     Register
                 </AuthButton>
                 </Spacer>
