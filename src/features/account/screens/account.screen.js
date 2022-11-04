bf7418ae5746f74878c3e701988366281ac1d0a3
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-04 11:28:23 -0500

    fixed login errors

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,27 +1,37 @@
 import React from "react";
-import { AccountBackground, AccountContainer, AccountCover, AuthButton  } from "../components/account.styles";
+
 import { Spacer } from "../../../components/spacer/spacer.component";
-    
-    
-export const AccountScreen = ({navigation}) => {
-    return <AccountBackground> 
-        <AccountCover />
-        <AccountContainer>
-            <AuthButton icon="lock-open-outline"
-                mode="contained"
-                onPress={() =>
-                navigation.navigate('Login')}>
-    Login
-            </AuthButton>
-            <Spacer size="large">
-                <AuthButton
-                    icon="email"
-                    mode="contained"
-                    onPress={() =>
-                    navigation.navigate('Register')}>
-    Register
-                </AuthButton>
-                </Spacer>
-        </AccountContainer>
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+  Title,
+} from "../components/account.styles";
+
+export const AccountScreen = ({ navigation }) => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Title>Meals To Go</Title>
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate("Login")}
+        >
+          Login
+        </AuthButton>
+        <Spacer size="large">
+          <AuthButton
+            icon="email"
+            mode="contained"
+            onPress={() => navigation.navigate("Register")}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
     </AccountBackground>
+  );
 };
